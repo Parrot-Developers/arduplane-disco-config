1bee76b825215b5392df72165c35cf25657ea00e
Nicolas Carrier <nicolas.carrier@parrot.com>
2016-09-14 16:19:54 +0200

    [DEV] added "./build.sh update" command

--- build.sh
+++ build.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+set -e
+
+if [ "$1" = "update" ]; then
+    echo "updating filesystem"
+    drone_address=${DRONE_ADDRESS:-192.168.42.1}
+    rsync \
+        --partial --progress \
+        --archive \
+        --verbose \
+        --compress \
+        --no-owner \
+        --no-group \
+        Alchemy-out/linux-parrot-arm/final/ \
+        ${drone_address}::root/ \
+        --exclude-from=./config/rsync_exclude
+    adb shell chmod 640 '/etc/boxinit.d/*.rc'
+    exit 0
+fi
+
 if [ -n "$(which ccache)" ]; then
     export ALCHEMY_USE_CCACHE=1
 fi
