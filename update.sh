da8a09ee7e850c6cdcefec01cf3456102adb2907
Nicolas Carrier <nicolas.carrier@parrot.com>
2016-09-23 15:53:14 +0200

    [DEV] create the APM dir and be verbose to see why update.sh prints "Error"

--- update.sh
+++ update.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-/usr/bin/ftp -in << EOF
+/usr/bin/ftp -vdin << EOF
 open 192.168.42.1
 binary
+mkdir  internal_000/APM/
 put Alchemy-out/linux-parrot-arm/final/usr/bin/apm-plane-disco internal_000/APM/apm-plane-disco
 EOF
