fe552b4651fe8ddd332f7e59253601c6ded83c40
Nicolas Carrier <nicolas.carrier@parrot.com>
2016-09-23 15:11:26 +0200

    [DEV] removed update action of build.sh

--- build.sh
+++ build.sh
@@ -2,25 +2,6 @@
 
 set -e
 
-if [ "$1" = "update" ]; then
-    echo "updating filesystem"
-    drone_address=${DRONE_ADDRESS:-192.168.42.1}
-    adb remount
-    rsync \
-        --partial --progress \
-        --archive \
-        --verbose \
-        --compress \
-        --no-owner \
-        --no-group \
-        Alchemy-out/linux-parrot-arm/final/ \
-        ${drone_address}::root/ \
-        --exclude-from=./config/rsync_exclude
-    adb shell chmod 640 '/etc/boxinit.d/*.rc'
-    adb shell sync
-    exit 0
-fi
-
 if [ -n "$(which ccache)" ]; then
     export ALCHEMY_USE_CCACHE=1
 fi
